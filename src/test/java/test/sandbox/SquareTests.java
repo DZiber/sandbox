55f9a1d00242ad8b472e43d813c2399783fd3841
DZiber <dziber11@gmail.com>
2018-03-12 20:13:04 +0300

    Creating first tests

---
+++ src/test/java/test/sandbox/SquareTests.java
@@ -0,0 +1,16 @@
+package test.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by ZeBr on 12.02.2018.
+ */
+public class SquareTests {
+
+    @Test
+    public void testArea () {
+        Square s = new Square(5);
+        Assert.assertEquals(s.area(), 25.0);
+    }
+}
